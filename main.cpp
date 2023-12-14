a25b5a61c7025f941d82042f224dcf8e2dc5e070
Luxdragon <kopse@e.email>
2023-12-14 13:15:32 +0100

    Chi^2 muss noch korrigiert werden

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include "src/malta.h"
+#include <cmath>
 
 //Compile with g++ main.cpp src/malta.cpp -o malta
 
 double integrand(double x) {
     return x * x;
+    //return x *x * x;
+    //return exp(x);
+    //return log(x);
 }
 
 int main() {
     Malta malta;
-    double result = malta.integrate(integrand, 5, 42, 1000);
+    double result = malta.integrate(integrand, 5, 42, 10000);
     std::cout << "Endergebnis:\t" << result << std::endl;
     return 0;
 }
