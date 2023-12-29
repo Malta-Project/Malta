297b92f75a252995bef625fb31fc5a3f34fc66aa
Nabil <nabil.salama@outlook.de>
2023-12-29 15:55:53 +0100

    time measurement

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #define MAIN_CPP
 
 #include <iostream>
+#include <chrono>
 #include "malta.h"
 
 using namespace std;
@@ -11,9 +12,12 @@ double integrand(double x) {
 }
 
 int main() {
-    Malta malta = Malta(200, 5, 200);
+    Malta malta = Malta(10000, 50, 100);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     malta.integrate(integrand, 0, 1);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     cout << "the result is I=" << malta.get_results() << endl;
+    std::cout << "Integration time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     return 0;
 }
 
