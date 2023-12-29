2f08f27b232fc29d0298e1abd578b3bae24b5701
XSileya <131592500+XSileya@users.noreply.github.com>
2023-12-29 21:21:56 +0100

    Update math.h

--- src/math.h
+++ src/math.h
@@ -1,10 +1,14 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <vector>
 class Math {
     public:
-        Math();
-        ~Math();
+        std::vector<int> static *get_random_points(int lower_limit, int upper_limit, int N);
+        std::vector<double> static *get_random_points(double lower_limit, double upper_limit, int N);
+        double static get_random_point(double lower_limit, double upper_limit);
+        int static get_random_point(int lower_limit, int upper_limit);
+        double static abs(double x);
 };
 
 #endif
