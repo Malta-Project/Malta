1ed0e3ac75c1f7e1a7a31d1a7e19c959e293f2e8
Nabil <nabil.salama@outlook.de>
2024-02-07 14:48:36 +0100

    integrand evaluatable

--- crosssection_topq.cpp
+++ crosssection_topq.cpp
@@ -15,10 +15,10 @@ double kaellen(double x, double y, double z) {
 }
 
 double pi = 3.14159265358979323846,
-       S = pow(14e3, 2), //GeV
+       S = pow(14e3, 2), //GeV^2
        m_t = 173, //GeV
        m_j = 0, //GeV
-       K_a1a2 = 1/36,
+       K_a1a2 = (double) 1/36,
        pT_min = 25, //GeV
        pT_max = 425, //GeV
        phist_min = 0,
@@ -36,7 +36,7 @@ double integrand(vector<double> x) {
            x1 = x[5];  
 
     double mT_j = sqrt(pow(pT,2) + pow(m_j,2));
-    double mT_stt = sqrt(pow(pT,2) + pow(s_tt,2));
+    double mT_stt = sqrt(pow(pT,2) + s_tt);
     double z_min = pow(mT_j + mT_stt, 2) / S;
     double x2 = z_min / (x1*z);
     double s = x1*x2*S;
@@ -48,7 +48,7 @@ double integrand(vector<double> x) {
     double Etst = sqrt(s_tt)/2;
     double ptst = sqrt(kaellen(s_tt, m2, m2)) / (2*sqrt(s_tt));
     double Ett = (s+s_tt-pow(m_j,2)) / (2*sqrt(s));
-    double gamm = Ett / sqrt (s_tt);
+    double gamm = Ett / sqrt(s_tt);
     double ptt0 = Ett;
     double pttx = 0;
     double ptty = -pT;
@@ -60,31 +60,31 @@ double integrand(vector<double> x) {
     double Et = gamm*Etst - gamm*bx*ptst*cos(phist)*sin(thetast) 
                 -gamm*by*ptst*sin(phist)*sin(thetast)
                 -gamm*bz*ptst*cos(thetast);
-    double Etbar = gamm*Etst+ gamm*bx*ptst*cos(phist)*sin(thetast);
+    double Etbar = gamm*Etst + gamm*bx*ptst*cos(phist)*sin(thetast);
                    +gamm*by*ptst*sin(phist)*sin(thetast)
                    +gamm*bz*ptst*cos(thetast);
     double ptz = -gamm*bz*Etst;
-                 +(gamm-1)*ptst*cos(phist)*sin(thetast )*bx*bz/b2
-                 +(gamm-1)*ptst*sin(phist)*sin(thetast )*by*bz/b2
+                 +(gamm-1)*ptst*cos(phist)*sin(thetast)*bx*bz/b2
+                 +(gamm-1)*ptst*sin(phist)*sin(thetast)*by*bz/b2
                  +((gamm-1)*pow(bz, 2)/b2+1)*ptst*cos(thetast );
     double ptzbar = -gamm*bz* Etst
                     -(gamm-1)*ptst*cos(phist)*sin(thetast)*bx*bz/b2
                     -(gamm-1)*ptst*sin(phist)*sin(thetast)*by*bz/b2
-                    -((gamm-1)*pow(bz, 2)/b2+1)*ptst*cos(thetast);
+                    -((gamm-1)*pow(bz, 2)/b2 + 1)*ptst*cos(thetast);
     double T = m2-sqrt(s)*(Et-ptz);
     double U = m2-sqrt(s)*(Et+ptz);
     double T14 = m2-sqrt(s)*(Etbar-ptzbar);
     double T24 = m2-sqrt(s)*(Etbar+ptzbar);
     double S34 = s_tt;
-    double S = s;
+    double S_ = s;
     double MT = m_t;
     double Nc = 3;
     double alphas = 0.118;
     double GS = sqrt(alphas*4*pi);
 
     double integrand_val = -(1/z) * (z_min/(x1*z)) * (pT/(16*pow(pi, 2)*s_tt))
-                       * sqrt(kaellen(s_tt, m2, m2)/kaellen(s, s_tt, pow(mT_j, 2)))
-                       * (K_a1a2/(4*pi*s)) * matrix_element_top(S, T, U, T14, T24, S34, MT, GS, Nc);
+                           * sqrt(kaellen(s_tt, m2, m2)/kaellen(s, s_tt, pow(mT_j, 2)))
+                           * (K_a1a2/(4*pi*s)) * matrix_element_top(S_, T, U, T14, T24, S34, MT, GS, Nc);
 
     return integrand_val;
 }
@@ -124,10 +124,7 @@ double integrand_transformed(std::vector<double> x) { //integrand is jacobi-tran
 }
 
 int main() {
-    std::cout << "val=" << integrand_transformed({0.5, 0.5, 0.5, 0.5, 0.5, 0.5}) << std::endl;
-    //std::cout << pow(2, 2) << std::endl;
-    return 0;
-    Malta malta = Malta(4, 1000, 100, 50);
+    Malta malta = Malta(6, 100000, 200, 30);
     chrono::steady_clock::time_point begin = chrono::steady_clock::now();
     malta.integrate(integrand_transformed);
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
