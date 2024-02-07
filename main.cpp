8c732c270965dc7511189e58727fbd67cd68ae46
Nabil <nabil.salama@outlook.de>
2024-02-07 11:16:37 +0100

    set to 16 threads

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ double integrand(std::vector<double> x) {
 
 int main() {
     Malta malta = Malta(2, 100000, 20, 20);
-    malta.set_threads(1);
+    malta.set_threads(16);
     malta.integrate(integrand, {{0.0, 2.0}, {1.0, 4.0}});
     cout << "result I=" << malta.get_result() << endl;
     cout << "error sigma=" << malta.get_error() << endl;
